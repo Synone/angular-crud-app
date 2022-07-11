b7c523daadfbea7aa7f788d8c3c5142bc78ae3ef
Sony <syno3311@gmail.com>
2022-07-11 21:29:04 +0700

    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+﻿import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { AlertComponent } from './_components';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { fakeBackendProvider } from './_helpers';
+
+// used to create fake backend
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  declarations: [AppComponent, AlertComponent, HomeComponent],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    // fakeBackendProvider
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
